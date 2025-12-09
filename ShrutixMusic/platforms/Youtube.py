ff77b883aa489e896f564765eae31553e72faa0c
vipin-kumar735 <patelharimohan31@gmail.com>
2025-12-09 21:54:36 +0530

    🍁𝐕𝐈𝐏 𝐂𝐇𝐎𝐑 🍁

--- ShrutixMusic/platforms/Youtube.py
+++ ShrutixMusic/platforms/Youtube.py
@@ -423,20 +423,7 @@ class YouTubeAPI:
                     downloaded_file = stdout.decode().split("\n")[0]
                     direct = False
                 else:
-                   file_size = await check_file_size(link)
-                   if not file_size:
-                     print("None file Size")
-                     return
-                   total_size_mb = file_size / (1024 * 1024)
-                   if total_size_mb > 250:
-                     print(f"File size {total_size_mb:.2f} MB exceeds the 100MB limit.")
-                     return None
-                   direct = True
-                   downloaded_file = await loop.run_in_executor(None, video_dl)
-        else:
-            direct = True
-            downloaded_file = await loop.run_in_executor(None, audio_dl)
-        return downloaded_file, direct
+                   file_size = await check_f        return downloaded_file, direct
 
 
 # ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
