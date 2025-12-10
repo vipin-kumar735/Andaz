07a00f1bc5e2092e6cde21e83f491997f683d023
vipin-kumar735 <patelharimohan31@gmail.com>
2025-12-10 17:30:49 +0530

    🍁𝐕𝐈𝐏 𝐂𝐇𝐎𝐑 🍁

--- ShrutixMusic/plugins/bot/inline.py
+++ ShrutixMusic/plugins/bot/inline.py
@@ -3,7 +3,7 @@ from pyrogram.types import (
     InlineKeyboardMarkup,
     InlineQueryResultPhoto,
 )
-from youtubesearchpython.__future__ import VideosSearch
+from py_yt import VideosSearch
 
 from ShrutixMusic import nand
 from ShrutixMusic.utils.inlinequery import answer
